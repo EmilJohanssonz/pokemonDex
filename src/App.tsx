60dc380e73d22724f7131751092288f24406c6c3
Emil Johansson <Emilaik@gmail.com>
2025-03-04 22:29:42 +0100

    pokemon cards and button to prev and next

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-
-import './App.css'
+import { useState } from "react";
+import PokemonApi from "./api/pokemonApi";
 
 function App() {
+  const [pokemonID, setPokemonID] = useState(1);
 
   return (
-    <>
-
-    </>
-  )
+    <div>
+      <h1>Pokédex</h1>
+      {/* Use PokemonApi component and pass Pokémon ID */}
+      <PokemonApi PokemonNameID={pokemonID} />
+      <button onClick={() => setPokemonID(pokemonID - 1)} disabled={pokemonID <= 1}>Prev</button>
+      <button onClick={() => setPokemonID(pokemonID + 1)}>Next</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
