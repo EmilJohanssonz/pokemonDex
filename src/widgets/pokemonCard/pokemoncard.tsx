1f23bc9a244a2af1744784410b437b992f4d26ab
Emil Johansson <Emilaik@gmail.com>
2025-03-04 22:29:18 +0100

    the table for pokemon cards

---
+++ src/widgets/pokemonCard/pokemoncard.tsx
@@ -0,0 +1,29 @@
+import { Pokemon } from "../../types/type";
+
+interface PokemonCardProps {
+  pokemon: Pokemon;
+}
+
+const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+export default function PokemonCard({ pokemon }: PokemonCardProps) {
+  const hpStat = pokemon.stats.find((stat) => stat.stat.name === "hp");
+
+  return (
+    <div>
+      <h2>{pokemon.name.toUpperCase()}</h2>
+      <p>
+        Types:{" "}
+        {pokemon.types
+          .map((typeInfo) => capitalize(typeInfo.type.name))
+          .join(", ")}
+      </p>
+      <p>
+        Abilities:{" "}
+        {pokemon.abilities.map((a) => capitalize(a.ability.name)).join(", ")}
+      </p>
+      {hpStat && <p>HP: {hpStat.base_stat}</p>}
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+    </div>
+  );
+}
